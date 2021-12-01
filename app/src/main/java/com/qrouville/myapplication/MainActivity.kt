f6a3d628e76099047349cf075d289db276eaa3b5
quentinrouville <qrouville@gmail.com>
2021-12-01 19:24:03 +0100

    Remove comment

--- app/src/main/java/com/qrouville/myapplication/MainActivity.kt
+++ app/src/main/java/com/qrouville/myapplication/MainActivity.kt
@@ -10,15 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_gradient))
-
-        /*supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.toolbar_gradient))
-
-        findViewById<TextView>(R.id.product_name).text = products[0].name
-        findViewById<TextView>(R.id.product_brand).text = products[0].brand
-
-        findViewById<TextView>(R.id.product_nutriscore).text = "Nutriscore : ${products[0].nutriscore}"
-        findViewById<TextView>(R.id.product_calories).text = "${products[0].kiloCalories} kCal/part"
-
-        Picasso.get().load(products[0].url).into(findViewById<ImageView>(R.id.product_image))*/
     }
 }

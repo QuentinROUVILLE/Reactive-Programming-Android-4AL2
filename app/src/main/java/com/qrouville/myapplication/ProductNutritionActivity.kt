551f189735319382a7d5fdff2ea31352e22185e0
Quentin R <rouvilleq@gmail.com>
2022-01-30 21:49:18 +0100

    Fill ProductNutritionActivity

--- app/src/main/java/com/qrouville/myapplication/ProductNutritionActivity.kt
+++ app/src/main/java/com/qrouville/myapplication/ProductNutritionActivity.kt
@@ -1,9 +1,12 @@
 package com.qrouville.myapplication
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 
 class ProductNutritionActivity : Fragment() {
@@ -24,5 +27,35 @@ class ProductNutritionActivity : Fragment() {
 
         val detailsFragmentNutrition: ProductNutritionFragment = requireParentFragment().requireParentFragment() as ProductNutritionFragment
         val product: Product = ProductNutritionFragmentArgs.fromBundle(detailsFragmentNutrition.requireArguments()).product
+
+        view.findViewById<TextView>(R.id.nutrition_fat_text).text = getString(R.string.nutrition_infos_fat, product.nutritionFacts.fat.quantityPer100Grams.plus(product.nutritionFacts.fat.unit))
+        evaluateColor(view.findViewById(R.id.nutrition_fat_sticker), view.findViewById(R.id.nutrition_fat_description), product.nutritionFacts.fat, 3.0, 20.0)
+
+        view.findViewById<TextView>(R.id.nutrition_acid_text).text = getString(R.string.nutrition_infos_acid, product.nutritionFacts.saturatedFattyAcid.quantityPer100Grams.plus(product.nutritionFacts.saturatedFattyAcid.unit))
+        evaluateColor(view.findViewById(R.id.nutrition_acid_sticker), view.findViewById(R.id.nutrition_acid_description), product.nutritionFacts.saturatedFattyAcid, 1.5, 5.0)
+
+        view.findViewById<TextView>(R.id.nutrition_sugar_text).text = getString(R.string.nutrition_infos_sugar, product.nutritionFacts.sugar.quantityPer100Grams.plus(product.nutritionFacts.sugar.unit))
+        evaluateColor(view.findViewById(R.id.nutrition_sugar_sticker), view.findViewById(R.id.nutrition_sugar_description), product.nutritionFacts.sugar, 5.0, 12.5)
+
+        view.findViewById<TextView>(R.id.nutrition_salt_text).text = getString(R.string.nutrition_infos_salt, product.nutritionFacts.salt.quantityPer100Grams.plus(product.nutritionFacts.fat.unit))
+        evaluateColor(view.findViewById(R.id.nutrition_salt_sticker), view.findViewById(R.id.nutrition_salt_description), product.nutritionFacts.salt, 0.3, 1.5)
+    }
+
+    private fun evaluateColor(stickerView: View, descView: TextView, nutritionFactsItem: NutritionFactsItem, lowValue: Double, moderateValue: Double) {
+        val nutritionValue = nutritionFactsItem.quantityPer100Grams.replace(',', '.').toDouble()
+        when {
+            nutritionValue <= lowValue -> {
+                DrawableCompat.setTintList(stickerView.background, ColorStateList.valueOf(resources.getColor(R.color.nutrient_level_low)))
+                descView.text = resources.getString(R.string.nutrition_infos_value_low)
+            }
+            nutritionValue <= moderateValue -> {
+                DrawableCompat.setTintList(stickerView.background, ColorStateList.valueOf(resources.getColor(R.color.nutrient_level_moderate)))
+                descView.text = resources.getString(R.string.nutrition_infos_value_moderate)
+            }
+            else -> {
+                DrawableCompat.setTintList(stickerView.background, ColorStateList.valueOf(resources.getColor(R.color.nutrient_level_high)))
+                descView.text = resources.getString(R.string.nutrition_infos_value_high)
+            }
+        }
     }
 }
